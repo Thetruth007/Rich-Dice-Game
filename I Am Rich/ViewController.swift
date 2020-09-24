ca8939ba6011edadb07b4a67f0de6044fac7c33e
Romero <>
2020-09-24 11:22:14 -0600

    first commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Romero on 4/16/20.
+//  Copyright © 2020 Romero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
